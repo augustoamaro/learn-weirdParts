e5c6633c45c991b335a93bb58dd3b75fe0ec0a06
Augusto <augustoamaro@icloud.com>
2022-02-26 16:16:46 -0300

    Initial commit

---
+++ 2 - Conceptual/app.js
@@ -0,0 +1,15 @@
+function b() {
+    var myvar;
+    console.log(myvar); // undefined
+}
+
+function a() {
+    var myvar = 2;
+    console.log(myvar); // 2
+    b();
+}
+
+var myvar = 1;
+console.log(myvar) // 1
+a();
+console.log(myvar); // 1
